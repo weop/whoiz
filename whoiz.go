f1ab58482eaa8663ecc969f0dfe89d667adde199
weop <weop@users.noreply.github.com>
2019-02-24 20:13:17 +0500

    v.1.0.1

--- whoiz.go
+++ whoiz.go
@@ -2,12 +2,17 @@ package main
 
 import (
   "fmt"
+  "strings"
   "net"
+  "net/http"
+  "crypto/tls"
   "log"
+  "io/ioutil"
   "os"
   "bytes"
   "os/exec"
   "github.com/urfave/cli"
+  "golang.org/x/net/publicsuffix"
 )
 
 //////////
@@ -29,16 +34,60 @@ func main() {
   app.Action = func(c *cli.Context) error {
     domainName := string(c.Args().Get(0))
 
-    cmd := exec.Command("bash","-c","whois "+domainName+"| head -n10")
+	tldomain, _ := publicsuffix.EffectiveTLDPlusOne(domainName)
+	if ( len(tldomain) <= 1 ){
+		println ("Invalid domain provided!");
+		return nil; //Lets not proceed without a valid TLD
+	}
+	
+
+    cmd := exec.Command("bash","-c","whois "+tldomain+"| head -n10")
     //todo is there a golib for whois?
     cmdOutput := &bytes.Buffer{}
     cmd.Stdout = cmdOutput
+
     err := cmd.Run()
     if err != nil {
      os.Stderr.WriteString(err.Error())
     }
+    if bytes.Contains(cmdOutput.Bytes(), []byte("No whois")) {
+	   	println("No whois server known for the provided domain name!");
+		return nil;
+	}
     fmt.Printf("\nDomain Reg:\n")
     fmt.Print(string(cmdOutput.Bytes()))
+
+    fmt.Printf("\nHTTPS:\n")
+    http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+    res, err := http.Get("https://"+domainName)
+    defer res.Body.Close()
+   
+    if err != nil {
+		fmt.Printf("   Reply: Invalid Response!\n")
+    }else{
+	    if res.StatusCode >= 200 && res.StatusCode <= 299 {
+	        fmt.Printf("   Reply: Valid [%d]\n", res.StatusCode)
+	    } else {
+	        fmt.Printf("   Reply: Invalid [%d]\n", res.StatusCode)
+	    }    	
+	    //Capture Page Title
+	    htmlBytes, _ := ioutil.ReadAll(res.Body)
+	    htmlStr := string(htmlBytes)
+	    domStart := strings.Index(htmlStr, "<title>") //get title
+	    if domStart == -1 {
+			 fmt.Printf("   No Page Title Found.\n")
+	    }else{
+		    domStart += 7 //skips <title>
+		    domEnd := strings.Index(htmlStr, "</title>")  //get ending
+		    if domEnd == -1 {
+				 fmt.Printf("   Error in Title.\n")
+		    }else{
+				siteTitle := []byte(htmlStr[domStart:domEnd])
+				fmt.Printf("   Page Title: %s\n", siteTitle)	    	
+		    }
+	    }
+    }
+
 
 
     fmt.Printf("\nHost(s):\n")
